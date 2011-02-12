00d80ac180f35d9fe8c27ace990dc8227512265d
Ilya Chistyakov <ilchistyakov@gmail.com>
2011-02-13 00:45:52 +0300

    Add generate creatures

--- run.py
+++ run.py
@@ -5,7 +5,12 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import sys
+from creatures.herbivore import Herbivore
+from creatures.plant import Plant
+from creatures.predator import Predator
+from utils import Color
 from world import World
+from random import randint
 
 
 def InitGL(Width, Height):
@@ -36,18 +41,36 @@ def KeyPressed(*args):
 
 
 def main():
+    Color.init('./rgb.txt')
     world = World(10, 10)
+
+    for i in xrange(5):
+        creature = Predator(x=randint(0, world.cols - 1),
+                            y=randint(0, world.rows - 1))
+        world.add_creature(creature)
+
+    for i in xrange(5):
+        creature = Herbivore(x=randint(0, world.cols - 1),
+                             y=randint(0, world.rows - 1))
+        world.add_creature(creature)
+
+    for i in xrange(5):
+        creature = Plant(x=randint(0, world.cols - 1),
+                         y=randint(0, world.rows - 1))
+        world.add_creature(creature)
 
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
     glutInitWindowSize(400, 300)
     glutInitWindowPosition(0, 0)
     glutCreateWindow("Life")
-    glutDisplayFunc(world.DrawGLScene)
-    glutIdleFunc(world.DrawGLScene)
+    glutDisplayFunc(world.draw_gl_scene)
+    glutIdleFunc(world.draw_gl_scene)
     glutReshapeFunc(ReSizeGLScene)
     glutKeyboardFunc(KeyPressed)
     InitGL(400, 300)
     glutMainLoop()
 
-main()
+
+if __name__ == '__main__':
+    main()
